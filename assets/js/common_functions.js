2165d45937fa5d9ee89ad2fd5228e5b99f4fc550
Karim Aly <karimalyazzam@gmail.com>
2023-08-14 14:32:34 +0300

    Cleanup code and remove all duplications

---
+++ assets/js/common_functions.js
@@ -0,0 +1,78 @@
+function clearText() {
+    document.getElementById("mileage").value = "";
+    // ⚡⚓✅✍❌❎⭐🔍🔍🔃🛞⚠️🔧
+    document.getElementById("output").innerText = "";
+}
+
+async function getRequiredServiceActions() {
+    let mileageText = document.getElementById("mileage");
+    let outputText = document.getElementById("output");
+
+    var mileage = mileageText.value;
+    if (!mileage || !/^\d+$/.test(mileage)) {
+        mileageText.value = WrongInputPromptText;
+        await new Promise(r => setTimeout(r, 300));
+        mileageText.value = "";
+        return;
+    }
+
+    if (mileage < 5000) {
+        outputText.innerHTML = "<h3><strong>⚓" + NoMaintenanceRequiredPromptText + "</strong></h3>";
+        return;
+    }
+
+    mileage_rounded = mileage - (mileage % 5000);
+
+    var actionsList = "";
+    var inspectionsList = "";
+    var serviceCost = 0;
+
+    if (mileage >= 5000 && mileage_rounded % 5000 == 0 && mileage_rounded >= 5000) {
+        inspectionsList += ServiceActions5KmText;
+        serviceCost += 350;
+    }
+
+    if (mileage_rounded % 10000 == 0) {
+        actionsList += ServiceActions10KmText;
+        serviceCost += 1850;
+    }
+    if (mileage_rounded % 20000 == 0) {
+        actionsList += ServiceActions20KmText;
+        serviceCost += 2500;
+    }
+    if (mileage_rounded % 30000 == 0) {
+        actionsList += ServiceActions30KmText;
+        serviceCost += 800;
+    }
+    if (mileage_rounded % 40000 == 0) {
+        actionsList += ServiceActions40KmText;
+        serviceCost += 2700;
+    }
+    if (mileage_rounded % 100000 == 0) {
+        actionsList += ServiceActions100KmText;
+        serviceCost += 1500;
+    }
+
+    if (actionsList && inspectionsList) {
+        outputText.innerHTML = "<h3><strong>⚓" + RequiredServicesText + "</strong></h3>    " + actionsList.trimStart() + "\n<h3><strong>⚓" + InspectionsText + "</strong></h3>    " + inspectionsList.trimStart();
+    } else if (actionsList) {
+        outputText.innerHTML = "<h3><strong>⚓" + RequiredServicesText + "</strong></h3>    " + actionsList.trimStart();
+    } else if (inspectionsList) {
+        outputText.innerHTML = "<h3><strong>⚓" + InspectionsText + "</strong></h3>    " + inspectionsList.trimStart();
+    }
+
+    if (serviceCost > 0) {
+        outputText.innerHTML += "\n<h3><strong>⚓" + ServiceCostText + "</strong></h3>    <em>" + ServiceCostDisclaimerText + "</em>\n    💵" + serviceCost + " EGP";
+    }
+}
+
+// function to detect keypress event
+document.getElementById("mileage").addEventListener("keypress", function (event) {
+    // detect event key
+    if (event.keyCode == 13) {
+        console.log("Enter key is pressed");
+        let inputText = document.getElementById("mileage").value;
+        console.log(inputText);
+        getRequiredServiceActions();
+    }
+});
---
+++ assets/js/english_translations.js
@@ -0,0 +1,70 @@
+let ServiceActions5KmText = `
+    🔍Check Engine Oil level, color and smell
+    🔍Check Coolant level in the tank
+    🔍Check Brake/Clutch Oil level in the tank
+    🔍Check All Belts for any tears or cracks
+    🔍Check Actuator fluid level in the tank
+        ✍Note: Check after all fluid returns to the tank
+    🔍Check Actuator fluid tank seal and there's no fluid leak
+    🔍Check Suspension for any damage
+    🔍Lubricate and tighten Suspension components
+    🔍Check Tires Pressure (29 PSI/200KPa)
+    🔍Check Tires for any crack or cut
+    🔍Check Windshield washer fluid level in the tank
+    🔍Apply Rubber Protectant to tires and all rubber parts in car
+`;
+let ServiceActions10KmText = `
+    ✅Replace Engine Oil
+        ✍Recomended Specs: Viscosity: <em>0W20</em> API: <em>SP</em>
+        ✍Amount: <em>3100ml</em> including filter
+        ✍Tightening torque for drain plug: <em>35Nm</em>
+
+    ✅Replace Engine Oil Filter
+        ✍OEM Part Number: <a href="https://www.marutisuzuki.com/genuine-parts/oil-filter/16510m68k00">16510M68K00</a> / <a href="https://www.marutisuzuki.com/genuine-parts/oil-filter/16510m68k20">16510M68K20</a>
+        ✍Tightening torque for oil filter: <em>14Nm</em> or <em>3/4 turn</em>
+
+    ✅Rotate Tires
+        ✍Rear Tires move to the front diagonally
+        ✍Front Tires move to the rear in the same side
+`;
+let ServiceActions20KmText = `
+    ✅Replace Spark Plugs
+        ✍Recomended Specs: NGK LKR6F-10
+        ✍OEM Part Number: <a href="https://www.marutisuzuki.com/genuine-parts/spark-plug/09482m00636">09482M00636</a>
+        ✍Tightening torque: <em>18Nm</em>
+
+    ✅Replace Engine Intake Air Filter
+        ✍OEM Part Number: <a href="https://www.marutisuzuki.com/genuine-parts/air-filter/13780m68p01">13780M68P01</a>
+`;
+let ServiceActions30KmText = `
+    ✅Replace Brake/Clutch Fluid
+        ✍Recomended Specs: <em>DOT3/DOT4</em>
+        ✍Amount: <em>~500ml</em>
+`;
+let ServiceActions40KmText = `
+    ✅Replace Coolant Fluid
+        ✍Recomended Specs: OAT Pre-Mixed 50% Ethylene Glycol
+        ✍Amount: <em>4100ml</em>
+
+    ✅Replace Transmission Oil
+        ✍Recomended Specs: <em>75W</em>
+        ✍Amount: <em>1500ml</em>
+        ✍Tightening torque for level plug: <em>21Nm</em>
+
+    ✅Replace AC V-Belt
+        ✍OEM Part Number: <a href="https://www.marutisuzuki.com/genuine-parts/v-belt-4pk/95141m55r30">95141M55R30</a>
+
+    ✅Replace Water Pump Belt
+        ✍OEM Part Number: <a href="https://www.marutisuzuki.com/genuine-parts/belt-water-pump/17521m68p00">17521M68P00</a>
+`;
+let ServiceActions100KmText = `
+    ✅Replace Fuel Filter
+        ✍OEM Part Number: <a href="https://www.marutisuzuki.com/genuine-parts/fuel-filter-assembly/15410m68p00">15410M68P00</a>
+`;
+
+let WrongInputPromptText = "Wrong Input!";
+let NoMaintenanceRequiredPromptText = "No Maintenance Required!";
+let RequiredServicesText = "Required Services";
+let InspectionsText = "Inspections";
+let ServiceCostText = "Service Cost";
+let ServiceCostDisclaimerText = "Cost displayed below is just an approxiamte\n    For exact costs, Call Suzuki Service Center ...";
